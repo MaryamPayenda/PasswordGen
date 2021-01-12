4db5f52ef6b251bf022e79215b1d0c6f657e2f48
MaryamPayenda <maryampayanda12@gmail.com>
2021-01-12 16:37:52 +0100

    commited

---
+++ passwordGen/passwordGen.js
@@ -0,0 +1,9 @@
+//Password Generator
+var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!%&";
+var string_length = 30;
+var randomstring = "";
+for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum,rnum+1);
+}
+console.log(randomstring);
